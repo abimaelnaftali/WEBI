13ac00f8bb016f41bc0a54d118e0567a6021a63b
Fellipe Nogueira Milomem <fellipemilomem@hotmail.com>
2024-07-23 08:42:35 -0300

    Alterar a dom

--- projeto-1/CadastrarProduto/cadastrar_produto.js
+++ projeto-1/CadastrarProduto/cadastrar_produto.js
@@ -203,28 +203,112 @@ function renderProducts(products) {
 
 // Função para criar o "card" de produto, ou seja, como o produto vai ser exibido na listagem
 function createProductCard(product) {
-    // Essa parte aqui fica pra quem criou a página...
-    // ...porque eu vou demorar a entender como tá feito ali a visualização
     const productCard = document.createElement('div');
+    productCard.style.display = 'flex';
+    productCard.style.justifyContent = 'space-between';
+    productCard.style.borderBottom = '1px solid #E6E5E5';
+    productCard.style.marginBottom = '1.5rem';
+    productCard.style.paddingBottom = '1.5rem';
+    productCard.style.alignItems = 'top';
 
     // Criação do elemento aki, tipo adicionar a imagem, o texto, etc.
-    // tô fazendo de forma simples assim só pra testar
+
+    const productImage = document.createElement('img');
+    productImage.style.maxWidth = '4rem';
+    productImage.style.maxHeight = '4rem';
+
+    productImage.src = product.photo;
+
+    productCard.appendChild(productImage);
+
+    const divBox = document.createElement('div');
+    divBox.style.display = 'column';
+    divBox.style.alignItems = 'left';
+    divBox.style.justifyContent = 'left';
+    divBox.style.maxWidth = '50rem';
+
+    productCard.appendChild(divBox);
+
+    const divBtn = document.createElement('div');
+    divBtn.style.display = 'flex';
+    divBtn.style.justifyContent = 'space-between';
+    divBtn.style.alignItems = 'center';
+
     const name = document.createElement('p');
+    name.style.color = '#403937';
+    name.style.fontWeight = 'bold';
+    name.style.textAlign = 'top';
+
     name.textContent = product.name;
+
+    divBox.appendChild(name);
+
+    divBox.appendChild(divBtn);
+
     
     const btn_remove = document.createElement('button');
+    btn_remove.style.border = 'none';
+    btn_remove.style.borderRadius = '6px';
+    btn_remove.style.backgroundColor = 'red';
+    btn_remove.style.color = '#fff';
+    btn_remove.style.fontSize = '0.75rem';
+    btn_remove.style.textTransform = 'uppercase';
+    btn_remove.style.maxWidth = '6rem';
+    btn_remove.style.maxHeight = '2.5rem';
+    btn_remove.style.padding = '0.5rem 0.8rem';
+    btn_remove.style.display = 'flex';
+    btn_remove.style.alignItems = 'center';
+    btn_remove.style.transition = 'background-color 0.3s';
+    btn_remove.style.marginTop = '1rem';
+    btn_remove.style.marginRight = '1rem';
+
+
+    btn_remove.addEventListener('mouseover', function() {
+        btn_remove.style.backgroundColor = '#dc2626';
+    });
+
+    btn_remove.addEventListener('mouseout', function() {
+        btn_remove.style.backgroundColor = 'red';
+    });
+
+    divBtn.appendChild(btn_remove);
+
     btn_remove.setAttribute('type', 'button');
     btn_remove.setAttribute('onclick', `removeProduct('${product.id}')`);
     btn_remove.innerHTML = "Remover";
 
     const btn_update = document.createElement('button');
+
+    btn_update.style.border = 'none';
+    btn_update.style.borderRadius = '6px';
+    btn_update.style.backgroundColor = '#E6E5E5';
+    btn_update.style.color = '#574F4D';
+    btn_update.style.fontSize = '0.75rem';
+    btn_update.style.textTransform = 'uppercase';
+    btn_update.style.maxWidth = '6rem';
+    btn_update.style.maxHeight = '2.5rem';
+    btn_update.style.padding = '0.5rem 0.5rem';
+    btn_update.style.display = 'flex';
+    btn_update.style.alignItems = 'center';
+    btn_update.style.gap = '0.3rem';
+    btn_update.style.transition = 'background-color 0.3s';
+    btn_update.style.marginTop = '1rem';
+
+    btn_update.addEventListener('mouseover', function() {
+        btn_update.style.backgroundColor = '#D7D5D5';
+    });
+
+    btn_update.addEventListener('mouseout', function() {
+        btn_update.style.backgroundColor = '#E6E5E5';
+    });
+
+    divBtn.appendChild(btn_update);
+
     btn_update.setAttribute('type', 'button');
     btn_update.setAttribute('onclick', `changeForm('${product.id}')`);
     btn_update.innerHTML = "Editar";
 
-    productCard.append(name, btn_remove, btn_update);
-
-    return productCard; // e retorna o "card" do produto pra ser listado e aparecer na página
+    return productCard;
 }
 
 // URL's
