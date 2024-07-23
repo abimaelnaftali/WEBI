de191863af74e6605fea098d516eefe3005fb673
Keystone <keystonebarreto@gmail.com>
2024-07-23 14:28:38 -0300

    Página de produto dinâmica

---
+++ projeto-1/Produto/produto.js
@@ -0,0 +1,32 @@
+const productImage = document.getElementById('product-image');
+const productTitle = document.getElementById('product-title');
+const productPrice = document.getElementById('product-price');
+const productDescription = document.getElementById('product-description');
+const productParcels = document.getElementById('product-parcels');
+//const productKey = "";
+
+document.addEventListener('DOMContentLoaded', function () {
+    const params = new URLSearchParams(window.location.search);
+    productKey = params.get("key");
+    searchProduct(productKey);
+});
+
+function searchProduct(){
+    return fetch(`https://web01-miniprojeto04-default-rtdb.firebaseio.com/products/${productKey}.json`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Resposta de rede não foi ok');
+            }
+            return response.json();
+        })
+        .then(product => {
+            productImage.src = product.photo;
+            productTitle.innerHTML = product.name;
+            productPrice.innerHTML = product.price;
+            productDescription.innerHTML = product.description;
+            productParcels.innerHTML = `Em ate ${product.parcels}x sem juros`
+        })
+        .catch(error => {
+            console.error('Houve um problema ao recuperar informações do contato:', error);
+        });
+}
--- projeto-1/Produtos/produtos.js
+++ projeto-1/Produtos/produtos.js
@@ -1,73 +1,3 @@
-/*class Product {
-    constructor({ id, name, description, quantity, price, photo, parcels, size }) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.quantity = quantity;
-        this.price = price;
-        this.photo = photo;
-        this.parcels = parcels;
-        this.size = size;
-    }
-}
-
-const productList = document.getElementById('product-list');
-
-document.addEventListener('DOMContentLoaded', function () {
-    listProducts();
-});
-
-function listProducts() {
-    // Busca produtos no Firebase
-    fetchProducts()
-        .then(products => {
-            renderProducts(products);
-        })
-        .catch(error => {
-            console.error('Houve um problema ao buscar os produtos:', error);
-        });
-}
-
-// Função para buscar produtos no Firebase
-function fetchProducts() {
-    return fetch('https://web01-miniprojeto04-default-rtdb.firebaseio.com/products.json')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Resposta de rede não foi ok');
-            }
-            return response.json();
-        })
-        .then(products => {
-            const productsList = [];
-            for (let key in products) {
-                const product = new Product({
-                    id: key,
-                    name: products[key].name,
-                    description: products[key].description,
-                    quantity: products[key].quantity,
-                    price: products[key].price,
-                    photo: products[key].photo,
-                    parcels: products[key].parcels,
-                    //size: products[key].size
-                });
-
-                productsList.push(product);
-            }
-            return productsList;
-        });
-}
-
-// Função para renderizar produtos na página
-function renderProducts(products) {
-    // Limpa os produtos existentes
-    productList.innerHTML = '';
-
-    products.forEach(product => {
-        const productCard = createProductCard(product);
-        productList.appendChild(productCard); // adiciona o produto no elemento pai
-    });
-}*/
-
 // Função para criar o "card" de produto, ou seja, como o produto vai ser exibido na listagem
 function createProductCard(product) {
     const productCard = document.createElement('div');
@@ -78,7 +8,7 @@ function createProductCard(product) {
 
     const link_page = document.createElement('a');
     link_page.classList.add('card')
-    link_page.href = "";
+    link_page.href = `../Produto/index.html?key=${product.id}`;
     link_page.target = '_blank';
 
     const image = document.createElement('img');
